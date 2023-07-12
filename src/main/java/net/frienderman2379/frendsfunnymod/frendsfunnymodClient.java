2d0223ce33e7a9827e9b92eaadd2ef03cef468ca
Frienderman <gregoryjacobfox@Gmail.com>
2023-07-12 13:21:52 -0400

    renamed stuff
    made stuff credit me

---
+++ src/main/java/net/frienderman2379/frendsfunnymod/frendsfunnymodClient.java
@@ -0,0 +1,10 @@
+package net.frienderman2379.frendsfunnymod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class frendsfunnymodClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
